31a37150e0afd9dbce16888416e6f500f2f301ab
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-04-11 08:47:36 +0000

    task no.9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -5,17 +5,15 @@
  *@tree: pointer to the root node of the tree to measure the height
  *Return: value to return
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	int l_height, r_height;
 
 	if (tree == NULL)
-	{
 		return (0);
-	}
-
 	l_height = binary_tree_height(tree->left);
 	r_height = binary_tree_height(tree->right);
-
-	return (1 +(l_height > r_height ? l_height : r_height));
+	return (1 + (l_height > r_height ? l_height : r_height));
 }
+
