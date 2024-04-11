037f0a4493c97e99e3d3aa4034c44eb4bbf93ba3
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-04-11 09:21:54 +0000

    task no.14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,10 +1,11 @@
 #include "binary_trees.h"
 
 /**
- *
- *
- *
+ * right_height - calculates the height of the right subtree 
+ * @tree: pointer to first node in right subtree
+ * Return: height of right subtree
  */
+
 int right_height(binary_tree_t *tree)
 {
 	int r_height;
@@ -16,17 +17,17 @@ int right_height(binary_tree_t *tree)
 }
 
 /**
- *
- *
- *
+ * left_height - calculate the height of the left subtree
+ * @tree: pointer to first in left subtree
+ * Return: height of left subtree
  */
+
 int left_height(binary_tree_t *tree)
 {
 	int l_height;
 
 	if (tree == NULL)
 		return (0);
-
 	l_height = left_height(tree->left);
 	return (l_height) + 1;
 }
@@ -34,8 +35,9 @@ int left_height(binary_tree_t *tree)
 /**
  *binary_tree_balance - function measures the balance factor of a binary tree
  *@tree: pointer to the root node of the tree to measure the balance factor
- *Return: value to return
+ *Return: tree balance
  */
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int right, left;
@@ -44,6 +46,6 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 	right = right_height(tree->right);
 	left = left_height(tree->left);
-
 	return (left - right);
 }
+
