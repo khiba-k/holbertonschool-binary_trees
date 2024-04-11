04bcdc93e12fe0b088b4713a4b745ec175f5f017
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-04-11 09:09:44 +0000

    betty style of coding

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -10,10 +10,10 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return 0;
-	return (
+	return ((
 		binary_tree_leaves(tree->left) +
 		(tree->left == NULL && tree->right == NULL ? 1 : 0) +
 		binary_tree_leaves(tree->right)
-		);
+		));
 }
 
