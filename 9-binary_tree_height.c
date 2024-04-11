7ad2516212384836e4944d68903c2039a5786374
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-04-11 08:54:55 +0000

    added error handling

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,8 +12,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL && tree->parent == NULL))
 		return (0);
-	l_height = binary_tree_height(tree->left);
-	r_height = binary_tree_height(tree->right);
-	return (1 + (l_height > r_height ? l_height : r_height));
+	else if (tree->parent != NULL)
+	{
+		l_height = binary_tree_height(tree->left);
+		r_height = binary_tree_height(tree->right);
+		return (1 + (l_height > r_height ? l_height : r_height));
+	}
 }
 
