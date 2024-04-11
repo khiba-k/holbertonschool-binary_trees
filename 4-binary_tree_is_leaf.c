6aba8f44206ddf6a1df9dcfde800c6978bd26714
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-04-11 08:15:29 +0000

    task no.4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,10 +6,13 @@
  *@node: pointer to the node to check
  *Return: value to return
  */
+
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node->left == NULL && node->right == NULL)
 		return (1);
-
+	binary_tree_is_leaf(node->left);
+	binary_tree_is_leaf(node->right);
 	return (0);
 }
+
