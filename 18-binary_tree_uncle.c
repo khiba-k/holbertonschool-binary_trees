6d2371993589a4cff03776ac0b667aa9a446410f
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-04-11 12:52:01 +0000

    added error handling

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -17,22 +17,22 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		if (parent->right == NULL)
 			return (NULL);
 		return (parent->right);
-	} else
-	{
-		if (parent->left == NULL)
-			return (NULL);
-		return (parent->left);
 	}
+	if (parent->left == NULL)
+		return (NULL);
+	return (parent->left);
 }
 
 /**
- *binary_tree_t - function finds the uncle of a node
+ *binary_tree_uncle - function finds the uncle of a node
  *@node: pointer to the node to find the uncle
  *Return: value to return
  */
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	if (node == NULL)
+		return (NULL);
 	return (binary_tree_sibling(node->parent));
 }
 
