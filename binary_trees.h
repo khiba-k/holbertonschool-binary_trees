8652d61e087274d9cc9eb0bc3b140c8cd8693cb8
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-04-11 07:23:26 +0000

    all tasks

--- binary_trees.h
+++ binary_trees.h
@@ -46,6 +46,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *SetNewNode_Values(int value);
 
 binary_tree_t *SetNewNode_Values(int value)
 {
@@ -62,7 +63,6 @@ int right_height(binary_tree_t *tree);
 int left_height(binary_tree_t *tree);
 size_t binary_tree_children(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
-void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
 
