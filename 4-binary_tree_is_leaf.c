c862f91914c8eec6d4ba411fbd9f68176d7dba92
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-04-11 08:21:18 +0000

    added error handling

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -11,7 +11,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-	if (node->left == NULL && node->right == NULL && node->parent != NULL)
+	if (node->left == NULL && node->right == NULL && (node->parent != NULL || node->parent == NULL))
 		return (1);
 	binary_tree_is_leaf(node->left);
 	binary_tree_is_leaf(node->right);
