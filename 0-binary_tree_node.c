792fd047f3ef220129c52fff3f2fb3794700ab71
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-04-11 07:47:20 +0000

    fix valgrind error

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -28,7 +28,7 @@ binary_tree_t *SetNewNode(int value)
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *Leaf = NULL, *New = NULL;
+	binary_tree_t *Leaf, *New;
 
 	if (parent == NULL)
 	{
